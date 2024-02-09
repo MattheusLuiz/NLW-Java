60f316363b86ef52c28f99461e3d40b7259dd301
Mattheus Luiz <matth.luiz10@gmail.com>
2024-02-09 19:31:10 -0300

    Projeto Finalizado

---
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/StudentController.java
@@ -0,0 +1,49 @@
+package com.rocketseat.certification_nlw.modules.students.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rocketseat.certification_nlw.modules.students.dto.StudentCertificationAnswerDTO;
+import com.rocketseat.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.certification_nlw.modules.students.entities.CertificationStudentEntity;
+import com.rocketseat.certification_nlw.modules.students.useCases.StudentCertificationAnswersUseCase;
+import com.rocketseat.certification_nlw.modules.students.useCases.VerifyIfHasCertificationUseCase;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    // Preciso usar o meu USECASE
+    @Autowired
+    private VerifyIfHasCertificationUseCase verifyIfHasCertificationUseCase;
+
+    @Autowired
+    private StudentCertificationAnswersUseCase studentCertificationAnswersUseCase;
+
+    @PostMapping("/verifyIfHasCertification")
+    public String verifyIfHasCertification(@RequestBody VerifyHasCertificationDTO VerifyHasCertificationDTO) {
+        // EMAIL
+        // TECHNOLOGY
+        var result = this.verifyIfHasCertificationUseCase.execute(VerifyHasCertificationDTO);
+        if (result) {
+            return "Usuario ja fez a prova";
+        }
+        return "Usuario pode fazer a prova";
+    }
+
+    @PostMapping("/certification/answer")
+    public ResponseEntity<Object> certificationAnswer(
+            @RequestBody StudentCertificationAnswerDTO studentCertificationAnswerDTO) throws Exception {
+        try {
+            var result = studentCertificationAnswersUseCase.execute(studentCertificationAnswerDTO);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
+}
